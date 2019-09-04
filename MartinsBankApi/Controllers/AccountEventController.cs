4c819af7204d5c7fd56318b2b76def627615d084
Lucas Martins <martins@itstransdata.com>
2019-09-04 11:24:37 -0300

    primeiro commit

---
+++ MartinsBankApi/Controllers/AccountEventController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MartinsBankApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountEventController : ControllerBase
+    {
+        
+        [HttpPost( "account/{accountId}/credit" )]
+        public IActionResult PostCredit( [FromRoute]int accountId )
+        {
+            return Ok( );
+        }
+
+        [HttpPost( "account/{accountId}/debt" )]
+        public IActionResult PostDebt( [FromRoute]int accountId )
+        {
+            return Ok( );
+        }
+    }
+}
---
+++ MartinsBankApi/Middlewares/SwaggerMeddleware.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.PlatformAbstractions;
+using Swashbuckle.AspNetCore.Swagger;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MartinsBankApi.Middlewares
+{
+    public static class SwaggerMeddleware
+    {
+        public static void AddSwaggerMiddleware(this IServiceCollection services )
+        {
+            services.AddSwaggerGen( ( a ) =>
+            {
+                a.SwaggerDoc( "v1", new Info
+                {
+                    Title = "Martins Bank",
+                    Version = "v1",
+                    Description = "Martins Bank API"
+                } );
+
+                var appPath = PlatformServices.Default.Application.ApplicationBasePath;
+
+                var xmlDocPath = Path.Combine( appPath, "MartinsBankApi.xml" );
+
+                a.IncludeXmlComments( xmlDocPath );
+            } );
+        }
+
+        public static void UseSwaggerMiddleware( this IApplicationBuilder app )
+        {
+            app.UseSwagger( );
+
+            app.UseSwaggerUI((a) => 
+            {
+                a.SwaggerEndpoint( "/swagger/v1/swagger.json", "MartinsBankApi" );
+
+                a.RoutePrefix = "docs";
+            } );
+        }
+    }
+}
